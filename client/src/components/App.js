f01d159a9bcdc535c8da39be327bd98f908cb5cb
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2021-02-03 11:14:35 -0500

    Image for front end

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -85,6 +85,7 @@ const App = () => {
               <h3>{entry.title}</h3>
               <p>{entry.comments}</p>
               <small>Visited on: {new Date(entry.visitDate).toLocaleDateString()}</small>
+              {entry.image && <img src={entry.image} alt={entry.title} />}
             </div>
           </Popup>
           ) : null
